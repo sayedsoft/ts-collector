c9f486f120493f6209d6f48fb01c4824c72fc3bb
sayedsoft <yemen@sayedsoft.com>
2023-08-04 17:36:30 +0300

    last

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,12 +1,30 @@
-interface IItemWithName {
-  name: string;
+declare module "collector" {
+  export interface IItemWithName {
+    name: string;
+  }
+
+  export interface IItemCollection<T> {
+    getAll(): T[];
+    get(name: string): T | undefined;
+    add(item: T): void;
+    remove(item: T): void;
+    has(item: T): boolean;
+    search(searchTerm: string): T[];
+  }
+
+  export class ItemCollection<T extends IItemWithName>
+    implements IItemCollection<T> {
+    private items: T[];
+
+    constructor();
+
+    getAll(): T[];
+    get(name: string): T | undefined;
+    add(item: T): void;
+    remove(item: T): void;
+    has(item: T): boolean;
+    search(searchTerm: string): T[];
+  }
 }
 
-interface IItemCollection<T> {
-  getAll(): T[];
-  get(name: string): T | undefined;
-  add(item: T): void;
-  remove(item: T): void;
-  has(item: T): boolean;
-  search(searchTerm: string): T[];
-}
+export {};
